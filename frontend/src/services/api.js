6273275c0c8ec154db22ac480ea04afb4ef2c59f
Adrian-Rubio <adrian41198@gmail.com>
2026-02-20 09:45:57 +0100

    Fix API_URL for production (Ubuntu server)

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'http://192.168.0.70:8000/api';
 
 export const fetchFilterOptions = async () => {
     try {
